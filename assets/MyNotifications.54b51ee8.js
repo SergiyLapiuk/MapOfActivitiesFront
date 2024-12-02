9ad6838282be746eda8b1920fd5179448ed0b04f
SergiyLapiuk <sergiy.lapiuk@knu.ua>
2024-12-02 04:09:39 +0200

    6443b97

---
+++ assets/MyNotifications.54b51ee8.js
@@ -0,0 +1,1 @@
+import{_ as e,J as t,V as a}from"./index.39a34321.js";const r={data(){return{messages:[]}}};function s(o,c,n,_,i,p){return t(),a("div")}var d=e(r,[["render",s]]);export{d as default};
